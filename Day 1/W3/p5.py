be19bf1d2e4b2a4f4849d5c9d8a33a39a00f2d65
CatPhD <34811818+CatPhD@users.noreply.github.com>
2017-12-25 01:10:53 +0900

    Delete p5.py

--- Day 1/W3/p5.py
+++
@@ -1,7 +0,0 @@
-Level = int(input("소득분위 입력(1~10분위):"))
-CulSem = int(input("현재 누적학기:"))
-Grade = float(input("학기 평점:"))
-
-
-R = (Level==1 and Grade >= 3.7) or (Level <=3 and CulSem <8 and Grade >=4.0) 
-print("장학금 대상자:", R)
