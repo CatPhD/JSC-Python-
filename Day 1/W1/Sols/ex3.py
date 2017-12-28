99e160920d7b9de7288bf28a2d3c97ce980edbc0
CatPhD <34811818+CatPhD@users.noreply.github.com>
2017-12-28 12:16:03 +0900

    Add files via upload

---
+++ Day 1/W1/Sols/ex3.py
@@ -0,0 +1,3 @@
+print("{} kg => {:.1f} pounds".format(63, 63/0.45359))
+print("{} cm => {:.1f} feet".format(173, 173/30.48))
+print("{} C => {:.1f} F".format(36.5, 36.5*9/5+32))
---
+++ Day 1/W1/Sols/ex4.py
@@ -0,0 +1,4 @@
+print('{:^5}|{:^10}'.format("R", "Area"))
+print('{:5}|{:>10}'.format(1, format(3.141592*1**2, '.1f')))
+print('{:5}|{:>10}'.format(5, format(3.141592*5**2, '.1f')))
+print('{:5}|{:>10}'.format(10, format(3.141592*10**2, '.1f')))
