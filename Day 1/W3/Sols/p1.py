9beef7846c6422608cb93aee07ab43170faeb093
CatPhD <34811818+CatPhD@users.noreply.github.com>
2017-12-28 12:18:46 +0900

    Add files via upload

---
+++ Day 1/W3/Sols/p1.py
@@ -0,0 +1,3 @@
+INT = int(input("Enter an integer: "))
+
+print( (-2 <= INT) and (INT <=3) and (INT!=0))
---
+++ Day 1/W3/Sols/p3.py
@@ -0,0 +1,2 @@
+Grade = input("Enter a letter grade: ")
+print(Grade in ('A', 'B', 'C', 'D', 'F', 'a', 'b', 'c', 'd', 'f'))
