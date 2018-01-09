d5063e14080a46dbeaf78ef242cd427763202200
CatPhD <34811818+CatPhD@users.noreply.github.com>
2018-01-09 18:21:48 +0900

    Delete p1.py

--- Day 5/W9/p1.py
+++
@@ -1,9 +0,0 @@
-import re
-
-pattern = r'01\d-\d{3,4}-\d{4}'
-string = input('Enter a phone number: ')
-match = re.findall(pattern, string)
-if len(match) == 1:
-    print('Correct!')
-else:
-    print('Incorrect!')
